6936740aad43545187f60d7bc62c827a41943476
roblesdotdev <httpsnip@gmail.com>
2023-12-18 12:12:17 -0300

    fix: static analysis

--- app/src/main/java/com/roblesdotdev/jetdo/ui/theme/Color.kt
+++ app/src/main/java/com/roblesdotdev/jetdo/ui/theme/Color.kt
@@ -66,5 +66,4 @@ val md_theme_dark_surfaceTint = Color(0xFFB5C4FF)
 val md_theme_dark_outlineVariant = Color(0xFF45464F)
 val md_theme_dark_scrim = Color(0xFF000000)
 
-
 val seed = Color(0xFF082567)
--- app/src/main/java/com/roblesdotdev/jetdo/ui/theme/Theme.kt
+++ app/src/main/java/com/roblesdotdev/jetdo/ui/theme/Theme.kt
@@ -46,7 +46,7 @@ private val LightColors = lightColorScheme(
     inversePrimary = md_theme_light_inversePrimary,
     surfaceTint = md_theme_light_surfaceTint,
     outlineVariant = md_theme_light_outlineVariant,
-    scrim = md_theme_light_scrim,
+    scrim = md_theme_light_scrim
 )
 
 private val DarkColors = darkColorScheme(
@@ -78,7 +78,7 @@ private val DarkColors = darkColorScheme(
     inversePrimary = md_theme_dark_inversePrimary,
     surfaceTint = md_theme_dark_surfaceTint,
     outlineVariant = md_theme_dark_outlineVariant,
-    scrim = md_theme_dark_scrim,
+    scrim = md_theme_dark_scrim
 )
 
 @Composable
