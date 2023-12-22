c87ee37442c842388d0979f0533e8c4bb6906ddc
roblesdotdev <httpsnip@gmail.com>
2023-12-22 16:59:57 -0300

    simulate login success

--- app/src/main/java/com/roblesdotdev/jetdo/MainActivity.kt
+++ app/src/main/java/com/roblesdotdev/jetdo/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.roblesdotdev.jetdo
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.ui.platform.LocalContext
 import com.roblesdotdev.jetdo.core.ui.theme.JetDoTheme
 import com.roblesdotdev.jetdo.login.ui.LoginScreen
 import dagger.hilt.android.AndroidEntryPoint
@@ -13,7 +15,14 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JetDoTheme {
-                LoginScreen()
+                val localContext = LocalContext.current.applicationContext
+                LoginScreen(onLoginComplete = {
+                    Toast.makeText(
+                        localContext,
+                        "Login Success",
+                        Toast.LENGTH_LONG
+                    ).show()
+                })
             }
         }
     }
--- app/src/main/java/com/roblesdotdev/jetdo/login/ui/LoginScreen.kt
+++ app/src/main/java/com/roblesdotdev/jetdo/login/ui/LoginScreen.kt
@@ -1,17 +1,36 @@
 package com.roblesdotdev.jetdo.login.ui
 
+import android.widget.Toast
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.platform.LocalContext
 import androidx.hilt.navigation.compose.hiltViewModel
 
 @Composable
-fun LoginScreen(viewModel: LoginViewModel = hiltViewModel()) {
+fun LoginScreen(
+    viewModel: LoginViewModel = hiltViewModel(),
+    onLoginComplete: () -> Unit
+) {
     val state = viewModel.viewState.collectAsState()
+
+    DisposableEffect(state.value) {
+        if (state.value is LoginViewState.Completed) {
+            onLoginComplete.invoke()
+        }
+
+        onDispose { }
+    }
+
+    val context = LocalContext.current.applicationContext
+
     LoginContent(
         state = state.value,
         onEmailChanged = viewModel::emailChanged,
         onPasswordChanged = viewModel::passwordChanged,
         onLoginClicked = viewModel::loginButtonClicked,
-        onSignUpClicked = {}
+        onSignUpClicked = {
+            Toast.makeText(context, "Not implemented yet!", Toast.LENGTH_SHORT).show()
+        }
     )
 }
